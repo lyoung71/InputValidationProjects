d468496a757ce50c74f4ae062a168cf8fd8694df
Landon <landonyoung71@gmail.com>
2023-12-21 18:00:26 -0300

    Sandwich Maker

---
+++ SandwichMaker/README.md
@@ -0,0 +1,5 @@
+Description - A sandwhich-making program that uses the "PyInputPlus" module to allow users to build their own sandwiches with a series of inputs. Prices for each item are contained in a dictionary and are totaled at the end to provide the user with the total cost. 
+
+Instructions - Users will need to download and import the "pyinputplus" module by typing "pip install --user pyinputplus" into their terminal.
+
+Credits - The tutorial that I used for this project can be found in chapter eight of Al Sweigart's book "Automate the Boring Stuff with Python" (https://automatetheboringstuff.com/2e/chapter8/).
---
+++ SandwichMaker/sandwichMaker.py
@@ -0,0 +1,50 @@
+import pyinputplus as pyip
+
+
+def sandwichMaker():
+    prices = {
+        'white': 2.00, 'wheat': 3.00, 'sourdough': 3.50,
+        'chicken': 3.50, 'turkey': 4.50, 'ham': 3.00, 'tofu': 2.00,
+        'cheddar': 1.50, 'Swiss': 2.00, 'mozzarella': 1.50,
+        'mmlt': 1.00
+    }
+
+    cost = 0
+
+    bread = pyip.inputMenu(
+        ['white', 'wheat', 'sourdough'],
+        prompt="Which type of bread would you like?\n"
+    )
+    cost += prices[bread]
+
+    protein = pyip.inputMenu(
+        ['chicken', 'turkey', 'ham', 'tofu'],
+        prompt="Which type of protein would you like?\n"
+    )
+    cost += prices[protein]
+
+    cheese = pyip.inputYesNo(prompt="Would you like cheese?\n")
+
+    if cheese == "yes":
+        cheese_type = pyip.inputMenu(
+            ['cheddar', 'Swiss', 'mozzarella'],
+            prompt="Which type of cheese would you like?\n"
+        )
+        cost += prices[cheese_type]
+
+    mmlt = pyip.inputYesNo(
+        prompt="Would you like mayo, mustard, lettuce, or tomato?\n"
+    )
+    if mmlt == "yes":
+        cost += prices['mmlt']
+
+    numOfSandwiches = pyip.inputInt(
+        prompt="How many sandwiches?\n",
+        min=1
+    )
+
+    total_cost = float(cost * numOfSandwiches)
+
+    return f"Your total cost will be ${total_cost:.2f}"
+
+print(sandwichMaker())
